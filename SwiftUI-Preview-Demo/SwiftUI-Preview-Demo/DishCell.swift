efb1e655dfb7d7c0124fa856ec1da1162b2b806e
pitt500 <pedro.rojaslo50@gmail.com>
2019-12-13 16:00:54 -0700

    Add DishCell view

---
+++ SwiftUI-Preview-Demo/SwiftUI-Preview-Demo/DishCell.swift
@@ -0,0 +1,18 @@
+//
+//  DishCell.swift
+//  SwiftUI-Preview-Demo
+//
+//  Created by projas on 12/13/19.
+//  Copyright © 2019 projas. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+struct DishCell: View {
+  var body: some View {
+    Text("DishCell")
+  }
+}
+
+
